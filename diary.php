5487b37283d8d0eaf85c53be112daf282e82a574
Kevan Davis <kevan@kevan.org>
2020-11-30 22:27:23 +0000

    Add files via upload

---
+++ diary.php
@@ -0,0 +1,183 @@
+ANY DAY IN 2020
+===============
+
+Entry for NaNoGenMo 2020 by Kevan Davis.
+
+-------------------------------------------------------------------------------
+
+<?
+
+$entries = array();
+
+dearDiary("Took a walk around the neighbourhood.",20);
+dearDiary("Walked to some nearby woodland.",3);
+dearDiary("Went for a walk to the park.",3);
+dearDiary("Saw a kingfisher.",1);
+dearDiary("Saw a lesser-spotted woodpecker.",1);
+dearDiary("Saw a redwing.",1);
+dearDiary("Listened to the dawn chorus in the garden.",1);
+dearDiary("Attempted to detect bats.",3);
+dearDiary("Took a long walk and caught up on some podcasts.",3);
+dearDiary("Walked to the supermarket with the self-service tills to stock up on essentials.",25);
+dearDiary("Thought how I should walk more, I used to walk all the time.",10);
+dearDiary("Met up with a friend locally to walk around.",4);
+dearDiary("Caught a train a few miles to meet up with a friend in a park.",2);
+dearDiary("Had a friend visit for a barbecue in the garden.",1);
+dearDiary("Had a friend visit to sit in the garden for a few hours.",1);
+dearDiary("Had a visit from my mum, just sitting in the garden for the afternoon.",2);
+
+dearDiary("Tidied things up in the garden.",20);
+dearDiary("Weeded the garden.",20);
+dearDiary("Sorted out the vegetable beds in the garden.",6);
+dearDiary("Fixed something that had broken around the house.",5);
+dearDiary("Upgraded the broadband to a better connection.",1);
+dearDiary("Bought a new landline phone handset.",1);
+dearDiary("Bought myself a new laptop.",1);
+dearDiary("Bought a new printer cartridge.",1);
+dearDiary("Hung out some laundry.",15);
+dearDiary("Planted some stuff in the vegetable beds.",4);
+dearDiary("Dug some compost out of the compost heap.",1);
+dearDiary("Harvested some potatoes.",2);
+dearDiary("Harvested some tomatoes.",7);
+dearDiary("Harvested some chard.",7);
+dearDiary("Sat out in the garden for a while.",30);
+dearDiary("Built a small fire in the garden.",1);
+dearDiary("Painted the shed.",1);
+dearDiary("Mowed the lawn.",5);
+dearDiary("Built an A-frame for the vegetable bed.",1);
+dearDiary("Refilled the bird feeder.",2);
+dearDiary("Cleaned out the bird box.",1);
+dearDiary("Replaced a missing hook on the hammock.",1);
+dearDiary("Repotted a houseplant.",2);
+dearDiary("Killed some ants.",1);
+dearDiary("Killed some fungus gnats.",2);
+dearDiary("Baked a loaf of bread.",8);
+dearDiary("Baked some banana bread.",1);
+dearDiary("Made pancakes.",2);
+dearDiary("Made a roast.",6);
+dearDiary("Made popcorn.",5);
+dearDiary("Tried out a new recipe.",10);
+
+dearDiary("Huge lightning storm today.",2);
+dearDiary("Lots of fireworks in back gardens this evening.",2);
+
+dearDiary("Did some background reading for the new book.",30);
+dearDiary("Made some notes for the new book.",30);
+dearDiary("Did some writing on the new book.",30);
+dearDiary("Pitched a book idea.",1);
+dearDiary("Had an online meeting.",10);
+dearDiary("Caught up on a few personal emails.",30);
+dearDiary("Replied to some tech support emails.",30);
+dearDiary("Did some website maintenance.",30);
+dearDiary("Did some playtesting of a word game online.",20);
+dearDiary("Playtested a social deduction game online.",30);
+dearDiary("Worked on some game illustration.",2);
+dearDiary("Published a print-and-play word game.",1);
+dearDiary("Worked on a print-and-play board game for a games festival.",4);
+dearDiary("Launched a print-and-play board game for an online games festival.",1);
+
+dearDiary("Watched a film.",20);
+dearDiary("Watched some film I'd taped off the television.",25);
+dearDiary("Watched a stream of a theatre production.",3);
+dearDiary("Watched a stream of a music gig.",3);
+dearDiary("Watched a stream from a games festival.",2);
+dearDiary("Watched a quiz show.",10);
+dearDiary("Watched a detective show.",10);
+dearDiary("Watched a detective show. (I think I've seen this one before.)",1);
+dearDiary("Watched a science-fiction show.",20);
+dearDiary("Watched a documentary.",20);
+dearDiary("Watched a comedy show.",20);
+dearDiary("Watched some short videos.",90);
+dearDiary("Caught a few hours of a 24-hour comedy event.",4);
+dearDiary("Bought an album.",1);
+dearDiary("Listened to a podcast.",90);
+dearDiary("Listened to a quiz show.",12);
+dearDiary("Finished reading a book. I don't know why I'm reading so little this year.",6);
+
+dearDiary("Read the paper.",35);
+dearDiary("Read an interesting article.",70);
+dearDiary("Read too much political news.",30);
+dearDiary("Posted a photo of something online.",11);
+dearDiary("Made some moves in a game of nomic.",150);
+
+dearDiary("Played some videogames.",60);
+dearDiary("Played a board game.",35);
+dearDiary("Wrote some pages for a postal roleplaying game.",5);
+dearDiary("Did a cryptic crossword.",15);
+dearDiary("Played some boardgames online with friends.",30);
+dearDiary("Met up with a friend in an open-world video game.",30);
+dearDiary("Played a physical board game remotely with a friend.",10);
+dearDiary("Got a new board game in the post.",5);
+
+dearDiary("Wrote some interactive fiction code.",20);
+dearDiary("Gave up on the interactive fiction competition. Why did I start something that was entirely dialogue?",1);
+dearDiary("Made some notes about generating a 50,000 word novel.",5);
+dearDiary("Wrote some code to generate a 50,000 word novel.",10);
+dearDiary("Gave up on the generated novel, it isn't really coming together.",1);
+
+dearDiary("I'm tired of the government asking people to use common sense.",30);
+dearDiary("Still no idea whether we'll end up with a no-deal Brexit during a pandemic.",40);
+dearDiary("I hope this US election won't end badly.",40);
+
+dearDiary("^Slept badly last night.",15);
+dearDiary("^Had a dream where nobody was socially distancing.",5);
+dearDiary("^Dreamed about walking for miles.",10);
+dearDiary("Had a mild head cold.",3);
+dearDiary("I hope this cough is just a cough.",30);
+dearDiary("Neck feels stiff, I should be making more effort to move around.",20);
+dearDiary("Cut my hair.",2);
+
+dearDiary("Checked over the stockpile of long-life food I had ready a few months ago.",2);
+dearDiary("Ordered some toilet rolls in bulk, as it feels like people are about to start stockpiling them.",2);
+dearDiary("Got some masks in the post.",2);
+dearDiary("Took a rapid antibody test that I got through the post, with a pinprick drop of blood. The result was negative.",1);
+dearDiary("Attended a funeral remotely.",1);
+
+$date = strtotime("23 March 2020");
+
+$perday = sizeof($entries)/250;
+
+$words = 0;
+while ($words<50000)
+{
+	
+	$sentences = 0;
+	$leftovers = $perday-floor($perday);
+	
+	$writetoday = floor($perday);
+	if (rand(1,100)<$leftovers*100) { $writetoday++; }
+	
+	$entry = "";	
+	while ($sentences<$writetoday)
+	{
+		$sentence = $entries[array_rand($entries)];
+		
+		if (strpos($entry,$sentence)===false)
+		{
+			$entry .= $sentence." ";
+			$sentences++;
+			$entry = preg_replace("/^(.+)\^([^\.]+\. )/","$2$1",$entry);
+		}
+	}
+	
+	$entry = preg_replace("/\^/","",$entry);
+		
+	$entry = date('l \t\h\e jS \of F Y', $date)."\n\n".$entry."\n\n-------------------------------------------------------------------------------\n\n";
+
+	print wordwrap($entry, 80, "\n");
+	
+	$words += str_word_count($entry);
+
+	$date = strtotime("+1 day", $date);
+}
+
+print "[Generated ".sizeof($entries)." entries in $words words.]";
+
+function dearDiary($text,$count=1)
+{
+	global $entries;
+	
+	$entries = array_merge($entries, array_fill(1, $count, $text));
+}
+
+?>
